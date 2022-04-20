a26a8841dd65c4a105b8a5e1e35a02bcc705a47a
smarist <stmarisnwokike@gmail.com>
2022-04-21 10:00:47 +1200

    added some functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import {ReactComponent as DiceSvg} from "./images/icon-dice.svg"
+import {ReactComponent as DividerSvg} from "./images/pattern-divider-desktop.svg"
+
 
 function App() {
+  const [advice, setAdvice] = React.useState({})
+  const [start, setStart] = React.useState(false)
+
+  function dice(){
+    setStart(prevStart => !prevStart)
+  }
+
+  React.useEffect(()=> {
+    fetch("https://api.adviceslip.com/advice")
+    .then(res => res.json())
+    .then(data => setAdvice(data.slip.advice))
+  }, [start])
+
+
+
+console.log(advice)
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {
+         advice.length > 0?
+         <div className="app">
+           <div className='mini'>
+              <h1 className="advice-title">Advice </h1>
+              <p className='advice'>"{advice}."</p>
+           </div>
+            
+            <button onClick={()=> dice()}><DiceSvg className='svg'/></button>
+         </div>
+         : 
+          <div className='app'>
+            <div className='mini'>
+               <h1 className='advice-title'>Loading .....</h1>
+            </div>
+          </div>
+        
+      }
+      
+  </main>
   );
 }
 
