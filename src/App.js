388c5c37a6aede0ceae3a56c053261f4483d7300
smarist <stmarisnwokike@gmail.com>
2022-04-25 00:36:26 +1200

    made some changes to the loader

--- src/App.js
+++ src/App.js
@@ -4,38 +4,62 @@ import {ReactComponent as DiceSvg} from "./images/icon-dice.svg"
 import {ReactComponent as DividerSvg} from "./images/pattern-divider-desktop.svg"
 import {ReactComponent as DividerSvg1} from "./images/pattern-divider-mobile.svg"
 import { useMediaQuery } from 'react-responsive'
+import axios from 'axios'
+import Loader from './components/Loader';
 
 
 
 
 function App() {
-  const [advice, setAdvice] = React.useState([])
+  const [advice, setAdvice] = React.useState({
+    loading: false,
+    data: ""
+  })
   const [start, setStart] = React.useState(false)
 
   function dice(){
     setStart(prevStart => !prevStart)
   }
-
+/*
   React.useEffect(()=> {
     fetch("https://api.adviceslip.com/advice")
     .then(res => res.json())
     .then(data => setAdvice(data.slip.advice))
   }, [start])
 
+  */
 
+  React.useEffect(() => {
+    setAdvice({
+      ...advice,
+      loading: true,
+    })
+    axios.get("https://api.adviceslip.com/advice").then((response) => {
+      setAdvice({
+        ...advice,
+        loading: false,
+        data: response.data.slip.advice
+      });
+    });
+  }, [start]);
 
-  
   const isTabletOrMobile = useMediaQuery({ query: '(max-width:  900px)' })
   const isBigScreen = useMediaQuery({ query: '(min-width: 900px)' })
 
   return (
     <main>
       {
-         advice.length > 0?
+        advice.loading?
+        <Loader/> : ""
+      }
+
+
+      {
+         advice.data && !advice.loading?
          <div className="app">
            <div className='mini'>
               <h1 className="advice-title">Advice </h1>
-              <p className='advice'>"{advice}."</p>
+              <p className='advice'>"{advice.data}."</p>
               {isBigScreen && <p ><DividerSvg/></p>}
               {isTabletOrMobile && <p className='divider'><DividerSvg1/></p>}
               
@@ -44,13 +68,11 @@ function App() {
             <button onClick={()=> dice()}><DiceSvg className='svg'/></button>
          </div>
          : 
-          <div className='app'>
-            <div className='mini'>
-               <h1 className='advice-title'>Loading .....</h1>
-            </div>
-          </div>
-        
+               ""
+            
       }
+
+      
       
   </main>
   );
